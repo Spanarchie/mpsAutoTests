81e0f5f57c8d9df4e15ce0ab982813f68b097435
Colin Moore-Hill <spanarchiantesting@gmail.com>
2014-01-28 11:56:12 +0000

    This is initial Set up of environment
    Adding structure to testing.

---
+++ CrisCreate/mps_CrisCreate/src/main/java/MPS_AutoTest/mps_CrisCreate/App.java
@@ -0,0 +1,13 @@
+package MPS_AutoTest.mps_CrisCreate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
